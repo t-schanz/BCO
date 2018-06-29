8b113c8f7cfd49664ecf64ca709b7a441ad61256
Tobias Machnitzki <tobias.machnitzki@mpimet.mpg.de>
2018-06-29 18:09:26 +0200

    Configuring travis4

--- BCO/_tests/runAll.py
+++ BCO/_tests/runAll.py
@@ -1,5 +1,7 @@
 from BCO._tests import ClassTesting, ConverterTesting
+from datetime import datetime as dt
 
+clstst = ClassTesting(start=dt(2018,5,1),end=dt(2018,5,2),duration=1)
 
-ClassTesting.testEverything()
+clstst.testEverything()
 ConverterTesting()
