bc52a9f0a0fbd313a491a1a625a40f7857ec9e61
Tobias Machnitzki <tobias.machnitzki@mpimet.mpg.de>
2018-07-11 12:20:40 +0200

    Trying to fix installation issue with _version.py, now dity fix excluding the version

--- BCO/__init__.py
+++ BCO/__init__.py
@@ -32,22 +32,22 @@ FTP_SERVER = config["DEFAULT"]["SERVER_NAME"]
 # ----------------------------------------------------------
 # Setting the version:
 
-try:
-    version_path = os.path.split(package_directory)
-    print("Version: ", version_path)
-    print("Package: ", package_directory )
-    VERSIONFILE=os.path.join(version_path[0],"_version.py")
-    print("FILE: " ,VERSIONFILE)
-    verstrline = open(VERSIONFILE, "rt").read()
-except:
-    VERSIONFILE = os.path.join(package_directory, "_version.py")
-    verstrline = open(VERSIONFILE, "rt").read()
-VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"  # Pattern for finding the version string in the file _version.py
-mo = re.search(VSRE, verstrline, re.M)
-if mo:
-    __version__ = mo.group(1)
-else:
-    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+# try:
+#     version_path = os.path.split(package_directory)
+#     print("Version: ", version_path)
+#     print("Package: ", package_directory )
+#     VERSIONFILE=os.path.join(version_path[0],"_version.py")
+#     print("FILE: " ,VERSIONFILE)
+#     verstrline = open(VERSIONFILE, "rt").read()
+# except:
+#     VERSIONFILE = os.path.join(package_directory, "_version.py")
+#     verstrline = open(VERSIONFILE, "rt").read()
+# VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"  # Pattern for finding the version string in the file _version.py
+# mo = re.search(VSRE, verstrline, re.M)
+# if mo:
+#     __version__ = mo.group(1)
+# else:
+#     raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
 # -----------------------------------------------------------
 # Import all Modules
