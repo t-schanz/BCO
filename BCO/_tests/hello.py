32fe22839a90257a067e1bb972f2354c1c0fbd63
Tobias Machnitzki <tobias.machnitzki@mpimet.mpg.de>
2018-07-18 09:03:00 +0200

    Trying encryption in travis1

--- BCO/_tests/hello.py
+++ BCO/_tests/hello.py
@@ -1,5 +1,13 @@
 import os
 
 print("========================================")
-print(os.environ["test_key"])
+if os.environ["test_key"] == "test":
+    print("Key = Test")
+
+else:
+    print("Key is:")
+    foo = os.environ["test_key"]
+
+    if os.environ.has_key("test_key"):
+        print("But the key should be there.")
 print("========================================")
