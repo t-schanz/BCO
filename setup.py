8842398e390a074bd7c0cd60417c3df9e7abd9aa
darklefknight <darklefknight@web.de>
2018-07-19 10:55:30 +0200

    removed python_requires

--- setup.py
+++ setup.py
@@ -30,21 +30,25 @@ setup(name='BCO',
       url='https://github.com/tmachnitzki/BCO/',
 
       classifiers=[
-          # How mature is this project? Common values are
-          #   3 - Alpha
-          #   4 - Beta
-          #   5 - Production/Stable
-          'Development Status :: 3 - Alpha',
+              # How mature is this project? Common values are
+              #   3 - Alpha
+              #   4 - Beta
+              #   5 - Production/Stable
+              'Development Status :: 3 - Alpha',
 
-          # Indicate who your project is intended for
-          'Intended Audience :: Science/Research',
-          'Topic :: Scientific/Engineering :: Atmospheric Science',
+              # Indicate who your project is intended for
+              'Intended Audience :: Science/Research',
+              'Topic :: Scientific/Engineering :: Atmospheric Science',
 
-          # Specify the Python versions you support here. In particular, ensure
-          # that you indicate whether you support Python 2, Python 3 or both.
-          'Programming Language :: Python :: 3.6',
+              # Specify the Python versions you support here. In particular, ensure
+              # that you indicate whether you support Python 2, Python 3 or both.
+              'Programming Language :: Python :: 2',
+              'Programming Language :: Python :: 2.7',
+              'Programming Language :: Python :: 3',
+              'Programming Language :: Python :: 3.5',
+              'Programming Language :: Python :: 3.6',
 
-          'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+              'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
       ],
 
       # You can just specify the packages manually here if your project is
@@ -63,8 +67,6 @@ setup(name='BCO',
           'netCDF4>=1.1.1',
           'configparser>3'
       ],
-
-      python_requires='==2.7,>=3.5, <3.7',
 
       include_package_data=True,
 
